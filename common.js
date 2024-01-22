c19dae1d603eeea045330395f16b02270060d53d
kdk0781 <44281520+kdk0781@users.noreply.github.com>
2024-01-22 22:43:34 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 $(function(){
-	// Set the date we're counting down to
-	var countDownDate = new Date("Feb 5, 2024 15:00:00").getTime();
+	// Set the date we're counting down to 카운트다운 날짜 월 일 년도
+	var countDownDate = new Date("Feb 25, 2024 15:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
